78d9f288391605f258c7bad3888fb2b0f02169dd
Hector Fernando Ricardez Lara <hec_montemoon@hotmail.com>
2023-09-09 17:58:05 -0600

    Double plant features

---
+++ BedrockTools/src/Structure/Features/Minecraft/DoublePlantFeature.cs
@@ -0,0 +1,18 @@
+﻿using BedrockTools.Objects.Blocks;
+using BedrockTools.Objects.Blocks.Util;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BedrockTools.Structure.Features.Minecraft {
+    public class DoublePlantFeature : Feature {
+        public DoublePlantType PlantType { get; protected set; }
+        public DoublePlantFeature(DoublePlantType plantType) : base(1,2,1) {
+            PlantType = plantType;
+        }
+
+        public override Block GetBlock(int x, int y, int z) => VanillaBlockFactory.DoublePlant(PlantType,y==1);
+    }
+}
