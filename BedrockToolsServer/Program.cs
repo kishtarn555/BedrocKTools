12690c043f1b82911ee59d542a2a240194d71516
Hector Fernando Ricardez Lara <hec_montemoon@hotmail.com>
2023-10-20 14:00:53 -0600

    Bedroock Tools Server project

---
+++ BedrockToolsServer/Program.cs
@@ -0,0 +1,26 @@
+﻿using BedrockToolsServer;
+Console.WriteLine("===============");
+Console.WriteLine("Bedrock tools");
+Console.WriteLine("===============");
+Console.Out.Flush();
+if (args.Length < 1) {
+    Console.WriteLine("Start mode not specified in arguments");
+    return;
+}
+
+string executionMode = args[0];
+
+if (executionMode == "server") {
+
+    if (args.Length < 3) {
+        Console.WriteLine("Not enough arguments");
+        return;
+    }
+    string inputHandle = args[1];
+    string outputHandle = args[2];
+    
+    StructureServer server = new StructureServer(inputHandle, outputHandle);
+    
+    server.Start();
+    return;
+}
---
+++ BedrockToolsServer/StructureServer.cs
@@ -0,0 +1,76 @@
+﻿using BedrockTools.Nbt.Elements;
+using BedrockTools.Nbt.IO;
+using BedrockTools.Objects;
+using BedrockTools.Objects.Blocks;
+using BedrockTools.Structure;
+using BedrockTools.Structure.Features.Geometry;
+using System;
+using System.Collections.Generic;
+using System.IO.Pipes;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BedrockToolsServer {
+    internal class StructureServer {
+        public string InputHandle;
+        public string OutputHandle;
+        public bool Running {
+            get; protected set;
+        }
+
+        public StructureServer(string inputHandle, string outputHandle) {
+            InputHandle = inputHandle;
+            OutputHandle = outputHandle;
+            Running = false;
+        }
+
+        public void Start() {
+            using (var pipeRead = new AnonymousPipeClientStream(PipeDirection.In, InputHandle))
+            using (var pipeWrite = new AnonymousPipeClientStream(PipeDirection.Out, OutputHandle)) {
+                try {
+                    var sr = new StreamString(pipeRead);
+                    var sw = new StreamString(pipeWrite);
+                    Console.WriteLine("Server started");
+                    Running = true;
+                    while (Running) {
+                        if (!pipeRead.IsConnected || !pipeWrite.IsConnected) {
+                            Running = false;
+                            break;
+                        }
+                        Communicate(sr, sw,pipeRead, pipeWrite);
+                    }
+                    
+
+                }
+                catch (Exception ex) {
+                    throw;
+                }
+            }
+        }
+        void Communicate(StreamString sr, StreamString sw, AnonymousPipeClientStream pipeRead, AnonymousPipeClientStream pipeWrite) {
+            Console.WriteLine("Sending structure");
+            McStructure structure = new McStructure(new Dimensions(10, 10, 10));
+            SphereFeature sphere = new SphereFeature(structure.Size, FillMode.BorderThick, VanillaBlockFactory.Purpur());
+            sphere.PlaceInStructure(McTransform.Identity, structure);
+            NbtCompound nbt = new McStructureSerializer(structure).GetStructureAsNbt();
+            StringWriter stringWriter = new StringWriter();
+            new SNbtWriter(stringWriter).Write(nbt);
+
+            string response = stringWriter.ToString();
+            sw.WriteString(response);
+            pipeWrite.Flush();
+            Thread.Sleep(200);
+
+            string message = sr.ReadString();
+            Console.WriteLine("[Server] " + message);
+            Thread.Sleep(200);
+            sw.WriteString(message);
+            pipeWrite.Flush();
+
+          
+            
+        }
+
+    }
+}
