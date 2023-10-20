a2c7a9795a6070eb73b881269a1fae6f009d3092
Hector Fernando Ricardez Lara <hec_montemoon@hotmail.com>
2023-10-20 14:00:36 -0600

    Structure feature

--- BedrockTools/src/Structure/Features/StructureFeature.cs
+++ BedrockTools/src/Structure/Features/StructureFeature.cs
@@ -4,29 +4,14 @@ using BedrockTools.Objects;
 using BedrockTools.Objects.Entities;
 
 namespace BedrockTools.Structure.Features {
-    // FIXME: THIS DONT WORK
-    public class StructureFeature: McStructure, IMcStructure {
+    public class StructureFeature:Feature {
+
+        readonly IMcStructure Structure;
         
-        public StructureFeature(Dimensions size) : base(size) { }
-        public StructureFeature(Block[,,] blocks) : base(
-            new Dimensions(blocks.GetLength(0), blocks.GetLength(1), blocks.GetLength(2))
-        ) {
-            this.blocks = (Block[,,])blocks.Clone();
-            Size = new Dimensions(blocks.GetLength(0), blocks.GetLength(1), blocks.GetLength(2));
-        }      
+        public StructureFeature(IMcStructure structure) : base(structure.Size) {
+            Structure = structure;
+        }
 
-        //TODO: Rotation and mirroring
-        public void PlaceInStructure(IMcStructure structure, IntCoords offset) {
-            for (int x=0; x < Size.X; x++) {
-                for (int y = 0; y < Size.Y; y++) {
-                    for (int z=0; z < Size.Z; z++) {
-                        Block block = GetBlock(x, y, z);
-                        if (block!=null)
-                            structure.SetBlock(new IntCoords(x, y, z) + offset, blocks[x, y, z]);
-                    }
-
-                }
-            }
-        }       
+        public override Block GetBlock(int x, int y, int z) => Structure.GetBlock(x,y,z);
     }
 }
